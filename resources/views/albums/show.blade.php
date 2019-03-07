355628e59991cabdfae69abc91e8a5056dac662a
Igor Stojanovski <cableclark@gmail.com>
2019-03-07 15:26:51 +0100

    Photos album display added

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -8,4 +8,18 @@
     <br>
 <a href="/photos/create/{{$album->id}}" class="button is-primary">Upload</a>
 
+<div class="columns"> 
+    @if(count($album->photos) >0)
+        <section class="section">
+           @foreach ($album->photos as $photo)
+               <h3><b>{{$photo->title}} </b></h3>
+               
+               <a href= "/albums/{{$photo->id}}">
+               <img src="http://photo.com/storage/albums/{{$photo->album_id}}/{{$photo->photo}}" >
+               </a>
+               <p>{{$photo->description}}</p>
+           @endforeach
+       </section>
+     </div>
+     @endif
 @endsection
